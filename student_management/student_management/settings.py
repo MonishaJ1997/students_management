abdf230936c7124942018efebe8faa1ee58ccbf6
MonishaJ1997 <jkmonisha97@gmail.com>
2025-08-22 09:11:30 +0530

    Update settings.py

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+&jxy-8!g+h!6uu(!dn@cu0p4xsxct9^wfv3gov^&^ccg!29tx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
     
 
 
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
